e18fbdb15f5020553902db930d0aa0659dc64c66
soumyasreepaul2024 <156051650+soumyasreepaul2024@users.noreply.github.com>
2024-01-16 13:04:20 +0530

    Add files via upload

---
+++ About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './About.css'
+
+const About = () => {
+  console.log('Rendering About Page');
+  return (
+    <div>
+     
+      <p>This is the content of the About page helllooo</p>
+    </div>
+  );
+};
+
+export default About;
---
+++ Contact.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Contact = () => {
+  console.log('RENDERING CONTACT PAGE');
+  return (
+    <div>
+      <h1>Hello</h1>
+      <p>Hello ppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppp</p>
+      
+    </div>
+  )
+}
+
+export default Contact
